5f26cf56ff0ebb6216e56aee1c466649e8845b37
Dvel <git@dvel.me>
2022-11-29 18:23:53 +0800

    config: 提取日期时间的关键词配置到方案中； close #47

--- rime.lua
+++ rime.lua
@@ -2,9 +2,15 @@
 -------------------------------------------------------------
 -- 日期时间
 -- 提高权重的原因：因为在方案中设置了大于 1 的 initial_quality，导致 rq sj xq dt ts 产出的候选项在所有词语的最后。
-function date_translator(input, seg)
+function date_translator(input, seg, env)
+	local config = env.engine.schema.config
+	local date = config:get_string(env.name_space .."/date") or "rq"
+	local time = config:get_string(env.name_space .."/time") or "sj"
+	local week = config:get_string(env.name_space .."/week") or "xq"
+	local datetime = config:get_string(env.name_space .."/datetime") or "dt"
+	local timestamp = config:get_string(env.name_space .."/timestamp") or "ts"
     -- 日期
-    if (input == "rq") then
+    if (input == date) then
         local cand = Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "")
         cand.quality = 100
         yield(cand)
@@ -19,7 +25,7 @@ function date_translator(input, seg)
         yield(cand)
     end
     -- 时间
-    if (input == "sj") then
+    if (input == time) then
         local cand = Candidate("time", seg.start, seg._end, os.date("%H:%M"), "")
         cand.quality = 100
         yield(cand)
@@ -28,17 +34,20 @@ function date_translator(input, seg)
         yield(cand)
     end
     -- 星期
-    if (input == "xq") then
+    if (input == week) then
         local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
-        local cand = Candidate("week", seg.start, seg._end, "周" .. weakTab[tonumber(os.date("%w") + 1)], "")
+        local cand = Candidate("week", seg.start, seg._end, "星期" .. weakTab[tonumber(os.date("%w") + 1)], "")
         cand.quality = 100
         yield(cand)
-        local cand = Candidate("week", seg.start, seg._end, "星期" .. weakTab[tonumber(os.date("%w") + 1)], "")
+		local cand = Candidate("week", seg.start, seg._end, "礼拜" .. weakTab[tonumber(os.date("%w") + 1)], "")
+        cand.quality = 100
+        yield(cand)
+        local cand = Candidate("week", seg.start, seg._end, "周" .. weakTab[tonumber(os.date("%w") + 1)], "")
         cand.quality = 100
         yield(cand)
     end
     -- ISO 8601/RFC 3339 的时间格式 （固定东八区）（示例 2022-01-07T20:42:51+08:00）
-    if (input == "dt") then
+    if (input == datetime) then
         local cand = Candidate("datetime", seg.start, seg._end, os.date("%Y-%m-%dT%H:%M:%S+08:00"), "")
         cand.quality = 100
         yield(cand)
@@ -47,7 +56,7 @@ function date_translator(input, seg)
         yield(cand)
     end
     -- 时间戳（十位数，到秒，示例 1650861664）
-    if (input == "ts") then
+    if (input == timestamp) then
         local cand = Candidate("datetime", seg.start, seg._end, os.time(), "")
         cand.quality = 100
         yield(cand)
