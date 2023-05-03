3d43f3a2a8c8da2f0aab262420082e10cd8989e1
abcdefg233 <32760059+abcdefg233@users.noreply.github.com>
2023-05-04 05:04:23 +0800

    这是用户词吗？.lua
    为用户词添加一个*号的小提示

--- rime.lua
+++ rime.lua
@@ -1,3 +1,12 @@
+-------------------------------------------------------------
+-- 这是用户词吗？
+-- 非常简单的lua滤镜，为所有用户词都加上*作为提示
+function is_that_a_user_dict(input,env)
+ for cand in input:iter() do
+  if(string.find(cand.type,"user"))then cand.comment=cand.comment..'*'end
+  yield(cand)
+end end
+-------------------------------------------------------------
 -- Rime Lua 扩展 https://github.com/hchunhui/librime-lua
 -- 文档 https://github.com/hchunhui/librime-lua/wiki/Scripting
 -------------------------------------------------------------
