d33e1938c2e51c7036dfd3555fa631b619b0a10e
abcdefg233 <32760059+abcdefg233@users.noreply.github.com>
2023-05-04 21:31:00 +0800

    feat: lua_filter@is_in_user_dict (#244)

--- rime.lua
+++ rime.lua
@@ -302,3 +302,12 @@ function unicode(input, seg, env)
     end
 end
 -------------------------------------------------------------
+-- 为用户词典中（输入过）的内容结尾加上一个星号 *
+function is_in_user_dict(input, env)
+    for cand in input:iter() do
+        if (string.find(cand.type, "user")) then
+            cand.comment = cand.comment .. '*'
+        end
+        yield(cand)
+    end
+end
