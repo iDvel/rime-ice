7552237349ed12ea768c427f914df04bbca5149c
zhuchunyan <gmboomker@gmail.com>
2023-05-05 14:53:21 +0800

    feat: 删词, 隐藏词条, 词条降频

---
+++ lua/others/drop_words.lua
@@ -0,0 +1,7 @@
+drop_words = {
+	"苫次",
+	"艮园",
+	"牛屄",
+	"戢翼",
+}
+return drop_words
---
+++ lua/others/turndown_freq_words.lua
@@ -0,0 +1,10 @@
+local turndown_freq_words =
+{
+    ["咱体内"] = { "zantin", },
+    ["桥本换来"] = { "qnbfhrld", "qiaobenhuanlai", },
+    ["书丹"] = { "uudj", },
+    ["乾冰"] = { "gjbk", },
+    ["吉泽明步不"] = { "jizembb", },
+    ["乘虚"] = { "chengxu", },
+}
+return turndown_freq_words
