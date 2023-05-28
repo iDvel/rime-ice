93a004f1ef5f3800b92522f41c12f15f66e102da
Dvel <me@dvel.me>
2023-05-28 13:14:47 +0800

    去除末尾大写的方式

--- lua/autocap_filter.lua
+++ lua/autocap_filter.lua
@@ -1,14 +1,11 @@
 return function(input, env)
+	local code = env.engine.context.input -- 输入码
+	
     for cand in input:iter() do
-        local code = env.engine.context.input -- 输入码
-        local text = cand.text                -- 候选词
+		local text = cand.text            -- 候选词
 
-        -- 输入码结尾大写，候选词转换为大写
-        if #code > 2 and code:find("%u$") then
-            text = text:upper()
-            yield(Candidate(cand.type, 0, #code, text, cand.comment))
-        -- 除了输入码结尾大写做转换外，如果原词条包含大写字母，后面的方式将不做任何处理
-        elseif text:find("%u") then
+		-- 如果原词条包含大写字母，不做任何处理
+        if text:find("%u") then
             yield(cand)
         -- 输入码前两位大写，候选词转换为全大写
         elseif code:find("^%u%u+.*") then
