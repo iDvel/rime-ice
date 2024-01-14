47a87664400f48a2707284761f1517e9276ceb0f
Dvel <me@dvel.me>
2024-01-15 00:14:26 +0800

    feat: N 开头公历转农历 #565

--- lua/lunar.lua
+++ lua/lunar.lua
@@ -549,7 +549,7 @@ local function Date2LunarDate(Gregorian)
 	Month = tonumber(Gregorian.sub(Gregorian, 5, 6))
 	Day = tonumber(Gregorian.sub(Gregorian, 7, 8))
 	if Year > 2100 or Year < 1899 or Month > 12 or Month < 1 or Day < 1 or Day > 31 or string.len(Gregorian) < 8 then
-		return "无效日期"
+		return "无效日期", "无效日期"
 	end
 
 	-- 获取两百年内的农历数据
@@ -642,17 +642,29 @@ local function Date2LunarDate(Gregorian)
 end
 
 -- 农历
+-- 从 lunar: nl 获取农历触发关键字（双拼默认为 lunar）
+-- 从 recognizer/patterns/gregorian_to_lunar 获取第 2 个字符作为公历转农历的触发前缀，默认为 N
 local function translator(input, seg, env)
-	local date1, date2 = Date2LunarDate(os.date("%Y%m%d"))
 	env.lunar_key_word = env.lunar_key_word or
 		(env.engine.schema.config:get_string(env.name_space:gsub('^*', '')) or 'nl')
+	env.gregorian_to_lunar = env.gregorian_to_lunar or
+		(env.engine.schema.config:get_string('recognizer/patterns/gregorian_to_lunar'):sub(2, 2) or 'N')
 	if input == env.lunar_key_word then
-		local lunar_date = Candidate("", seg.start, seg._end, date1, "")
-		lunar_date.quality = 999
-		yield(lunar_date)
+		local date1, date2 = Date2LunarDate(os.date("%Y%m%d"))
 		local lunar_ymd = (Candidate("", seg.start, seg._end, date2, ""))
 		lunar_ymd.quality = 999
 		yield(lunar_ymd)
+		local lunar_date = Candidate("", seg.start, seg._end, date1, "")
+		lunar_date.quality = 999
+		yield(lunar_date)
+	elseif env.gregorian_to_lunar ~= '' and input:sub(1, 1) == env.gregorian_to_lunar then
+		local date1, date2 = Date2LunarDate(input:sub(2))
+		local lunar_ymd = (Candidate("", seg.start, seg._end, date2, ""))
+		lunar_ymd.quality = 999
+		yield(lunar_ymd)
+		local lunar_date = Candidate("", seg.start, seg._end, date1, "")
+		lunar_date.quality = 999
+		yield(lunar_date)
 	end
 end
 
