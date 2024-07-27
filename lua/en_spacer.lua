b528dc75d4906fe3ca0f14faf60c25cc3acd792b
mirtlecn <mirtle.cn+github@outlook.com>
2024-07-28 01:20:52 +0800

    chore: 简化 en_spacer 判断

--- lua/en_spacer.lua
+++ lua/en_spacer.lua
@@ -1,11 +1,11 @@
--- 中文或者英文后，再输入英文单词自动添加空格
+-- 英文后，再输入英文单词（必须为候选项）自动添加空格
 local F = {}
 
 function F.func( input, env )
     local latest_text = env.engine.context.commit_history:latest_text()
     for cand in input:iter() do
         if cand.text:match( '^[%a\']+[%a\']*$' ) and latest_text and #latest_text > 0 and
-            not latest_text:find( '[%p%s。，；？：—！《》‘’“”、¥…（）【】「」『』]$' ) then
+            latest_text:find( '^ ?[%a\']+[%a\']*$' ) then
             cand = cand:to_shadow_candidate( 'en_spacer', cand.text:gsub( '(%a+\'?%a*)', ' %1' ), cand.comment )
         end
         yield( cand )
@@ -13,4 +13,3 @@ function F.func( input, env )
 end
 
 return F
-
