2829984e37f0e64d66e4d432cb1fb8823e72a079
WantChane <wantchane@gmail.com>
2025-05-24 15:13:30 +0800

    fit: 以词定字,光标不在input末尾情况

--- lua/select_character.lua
+++ lua/select_character.lua
@@ -6,7 +6,7 @@
 -- 20240508111725 当候选字数为 1 时，快捷键使该字上屏
 -- 20250515093039 以词定字支持长句输入
 -- 20250516231523 当候选字数为 1 且还有未处理的输入时，快捷键使该字上屏, 保留未处理部分
-
+-- 20250524151149 当光标位置不在 input 末尾时,保留光标右侧部分
 local select = {}
 
 function select.init(env)
@@ -24,40 +24,35 @@ function select.func(key, env)
         and (context:is_composing() or context:has_menu())
         and (env.first_key or env.last_key)
     then
-        local candidate = context.input
-        if context:get_selected_candidate() then
-            candidate = context:get_selected_candidate().text
-        end
-        local selected = ""
-        if utf8.len(candidate) > 1 then
-            if (key:repr() == env.first_key) then
-                selected = candidate:sub(1, utf8.offset(candidate, 2) - 1)
-            elseif (key:repr() == env.last_key) then
-                selected = candidate:sub(utf8.offset(candidate, -1))
-            else
-                return 2
-            end
-        else
-            if key:repr() == env.first_key or key:repr() == env.last_key then
-                selected = candidate
-            else
-                return 2
-            end
-        end
-        local committed = context:get_commit_text()
-        local start_pos, end_pos = committed:find(candidate)
-        if start_pos and end_pos then
-            local part1 = committed:sub(1, end_pos):gsub(candidate, selected)
-            local part2 = committed:sub(end_pos + 1)
-            engine:commit_text(part1)
-            context:clear()
-            if part2 ~= "" then
-                context:push_input(part2)
-            end
-            return 1
+        local input = context.input
+        local selected_candidate = context:get_selected_candidate()
+        selected_candidate = selected_candidate and selected_candidate.text or input
+
+        local selected_char = ""
+        if (key:repr() == env.first_key) then
+            selected_char = selected_candidate:sub(1, utf8.offset(selected_candidate, 2) - 1)
+        elseif (key:repr() == env.last_key) then
+            selected_char = selected_candidate:sub(utf8.offset(selected_candidate, -1))
         else
             return 2
         end
+
+        local commit_text = context:get_commit_text()
+        local _, end_pos = commit_text:find(selected_candidate)
+        local caret_pos = context.caret_pos
+
+        local part1 = commit_text:sub(1, end_pos):gsub(selected_candidate, selected_char)
+        local part2 = commit_text:sub(end_pos + 1)
+
+        engine:commit_text(part1)
+        context:clear()
+        if caret_pos ~= #input then
+            part2 = part2 .. input:sub(caret_pos + 1)
+        end
+        if part2 ~= "" then
+            context:push_input(part2)
+        end
+        return 1
     end
     return 2
 end
