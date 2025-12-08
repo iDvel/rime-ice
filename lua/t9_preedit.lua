a07e7e4b413941719ae2a9b5c1fe26777713a121
Mirtle <mirtle@duck.com>
2025-12-08 14:19:19 +0800

    fix: lua 引入注释错误 close https://github.com/iDvel/rime-ice/issues/1405

--- lua/t9_preedit.lua
+++ lua/t9_preedit.lua
@@ -1,5 +1,5 @@
 -- 九宫格，将输入框的数字转为对应的拼音或英文，iRime 用，Hamster 不需要。
--- 在 engine/filters 增加 - lua_filter@t9_preedit
+-- 在 engine/filters 增加 - lua_filter@*t9_preedit
 --
 -- 九宫格专用，iRime 用，仓输入法不用
 -- 拼写规则通过 xlit 转写： xlit/abcdefghijklmnopqrstuvwxyz/22233344455566677778889999/
